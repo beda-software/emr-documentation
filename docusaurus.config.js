1f820b034249e615d68fc8a99ee4a3f01e1f771b
Dmitry Shutov <dmitry.s@beda.software>
2023-06-01 15:29:59 +0700

    Do not throw exceptions for broken links
    Just log it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'beda-software', // Usually your GitHub org/user name.
   projectName: 'emr-documentation', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
