5a36c8ceb5c89a8f81daa32dc2399b7646352e55
Yana Beda <troublegirl007@gmail.com>
2023-06-27 10:09:48 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/beda-software/emr-documentation/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/beda-software/emr-documentation/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/beda-software/emr-documentation/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/beda-software/emr-documentation/tree/main/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
